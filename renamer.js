23bb9c41a6a5c9b423bf721b396f00076f8463f8
kerbycourt <kerby.court.845@my.csun.edu>
2025-01-15 01:23:34 +0800

    Update renamer.js

--- renamer.js
+++ renamer.js
@@ -1,7 +1,8 @@
-const inArg = $arguments;
-const nm = inArg.nm || false;
+function operator(proxies = [], targetPlatform, context) {
+  const inArg = $arguments;
+  const nm = inArg.nm || false;
 
-const nameMap = {
+  const nameMap = {
     cn: "cn",
     zh: "cn",
     us: "us",
@@ -9,83 +10,82 @@ const nameMap = {
     quan: "quan",
     gq: "gq",
     flag: "gq"
-};
+  };
 
-const inname = nameMap[inArg.in] || "",
+  const inname = nameMap[inArg.in] || "",
     outputName = nameMap[inArg.out] || "";
 
-// Parallel arrays for different naming systems
-const FG = ['🇭🇰','🇲🇴','🇹🇼','🇯🇵','🇰🇷','🇸🇬','🇺🇸','🇬🇧'];
-const EN = ['HK','MO','TW','JP','KR','SG','US','GB'];
-const ZH = ['香港','澳门','台湾','日本','韩国','新加坡','美国','英国'];
-const QC = ['Hong Kong','Macao','Taiwan','Japan','Korea','Singapore','United States','United Kingdom'];
+  // Parallel arrays for different naming systems
+  const FG = ['🇭🇰','🇲🇴','🇹🇼','🇯🇵','🇰🇷','🇸🇬','🇺🇸','🇬🇧'];
+  const EN = ['HK','MO','TW','JP','KR','SG','US','GB'];
+  const ZH = ['香港','澳门','台湾','日本','韩国','新加坡','美国','英国'];
+  const QC = ['Hong Kong','Macao','Taiwan','Japan','Korea','Singapore','United States','United Kingdom'];
 
-function getList(arg) {
+  function getList(arg) {
     switch (arg) {
-        case 'us':
-            return EN;
-        case 'gq':
-            return FG;
-        case 'quan':
-            return QC;
-        default:
-            return ZH;
+      case 'us':
+        return EN;
+      case 'gq':
+        return FG;
+      case 'quan':
+        return QC;
+      default:
+        return ZH;
     }
-}
+  }
 
-function jxh(e) {
+  function jxh(e) {
     const n = e.reduce((e, n) => {
-        const t = e.find((e) => e.name === n.name);
-        if (t) {
-            t.count++;
-            t.items.push({
-                ...n,
-                name: `${n.name} ${t.count.toString().padStart(2, "0")}`
-            });
-        } else {
-            e.push({
-                name: n.name,
-                count: 1,
-                items: [{
-                    ...n,
-                    name: `${n.name} 01`
-                }]
-            });
-        }
-        return e;
+      const t = e.find((e) => e.name === n.name);
+      if (t) {
+        t.count++;
+        t.items.push({
+          ...n,
+          name: `${n.name} ${t.count.toString().padStart(2, "0")}`
+        });
+      } else {
+        e.push({
+          name: n.name,
+          count: 1,
+          items: [{
+            ...n,
+            name: `${n.name} 01`
+          }]
+        });
+      }
+      return e;
     }, []);
     
     const t = n.flatMap((e) => e.items);
     e.splice(0, e.length, ...t);
     return e;
-}
+  }
 
-function operator(pro) {
-    const Allmap = {};
-    const outList = getList(outputName);
-    let inputList = inname !== "" ? [getList(inname)] : [ZH, FG, QC, EN];
+  const Allmap = {};
+  const outList = getList(outputName);
+  let inputList = inname !== "" ? [getList(inname)] : [ZH, FG, QC, EN];
 
-    inputList.forEach((arr) => {
-        arr.forEach((value, valueIndex) => {
-            Allmap[value] = outList[valueIndex];
-        });
+  inputList.forEach((arr) => {
+    arr.forEach((value, valueIndex) => {
+      Allmap[value] = outList[valueIndex];
     });
+  });
 
-    pro.forEach((e) => {
-        let findKey = false;
-        for (const [key, value] of Object.entries(Allmap)) {
-            if (e.name.includes(key)) {
-                e.name = value;
-                findKey = true;
-                break;
-            }
-        }
-        if (!findKey && !nm) {
-            e.name = null;
-        }
-    });
+  proxies.forEach((proxy) => {
+    let findKey = false;
+    for (const [key, value] of Object.entries(Allmap)) {
+      if (proxy.name.includes(key)) {
+        proxy.name = value;
+        findKey = true;
+        break;
+      }
+    }
+    if (!findKey && !nm) {
+      proxy.name = null;
+    }
+  });
 
-    pro = pro.filter((e) => e.name !== null);
-    jxh(pro);
-    return pro;
+  const filteredProxies = proxies.filter((proxy) => proxy.name !== null);
+  jxh(filteredProxies);
+  return filteredProxies;
 }
